b2537b77d6787e7a4aee749b4e9ded3b87737a0c
Oleg Rubtsov <oleg@NOTE-orubtsov.gpmd.ru>
2024-06-02 16:08:28 +0300

    Homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,7 +4,7 @@
 """
 
 
-def power_numbers():
+def power_numbers(*num):
     return(list([nums ** 2 for nums in num]))
     """
     функция, которая принимает N целых чисел,
@@ -54,6 +54,3 @@ def filter_numbers(num, filter_type):
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
-filter_numbers([1, 2, 3], ODD)
-filter_numbers([2, 3, 4, 5], EVEN)
-filter_numbers([1, 9, 11, 2, 6, 9, 27, 23], PRIME)
