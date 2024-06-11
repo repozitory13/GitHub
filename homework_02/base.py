2d7409a097374e292ed983715e712e16bf40cfbb
repozitory13 <dark-horse@bk.ru>
2024-06-11 15:53:20 +0300

    Homework_01

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,22 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError
+from homework_02.exceptions import NotEnoughFuel
+from homework_02.exceptions import CargoOverload
 
 
 class Vehicle(ABC):
+    weight = 500
+    started = 0
+    fuel = 0
+    fuel_consumption = 10
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+    def start(self, started):
+        self.started = started
+        if self.started == 0
+            pass
+        raise ValueError("not")
     pass
+
