aa40bea86891edcc8951b523a7a171b28dfa3c96
repozitory13 <dark-horse@bk.ru>
2024-06-11 15:15:34 +0300

    Homework_01

---
+++ homework_02/exceptions.py
@@ -0,0 +1,18 @@
+"""
+Объявите следующие исключения:
+- LowFuelError 'вызывается когда мало топлива'
+- NotEnoughFuel 'вызывается, когда топливо закончилось'
+- CargoOverload 'вызывается при перегрузке'
+"""
+
+
+class LowFuelError(Exception):
+    pass
+
+
+class NotEnoughFuel(Exception):
+    pass
+
+
+class CargoOverload(Exception):
+    pass
